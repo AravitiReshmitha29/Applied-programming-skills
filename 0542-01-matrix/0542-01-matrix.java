ac7d8bce180c5c038daed0d172024949391e1231
AravitiReshmitha29 <vtu26450@veltech.edu.in>
2026-04-22 18:19:03 +0530

    Time: 13 ms (91.09%), Space: 48.9 MB (73.3%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        
+        Queue<int[]> queue = new LinkedList<>();
+        
+        // Step 1: Initialize
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                } else {
+                    mat[i][j] = -1; // mark unvisited
+                }
+            }
+        }
+        
+        // Directions: up, down, left, right
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        
+        // Step 2: BFS
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0], c = curr[1];
+            
+            for (int[] d : dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                
+                // check bounds and unvisited
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1) {
+                    mat[nr][nc] = mat[r][c] + 1;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+        
+        return mat;
+    }
+}
